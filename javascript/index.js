e275b8cf4a87610abb31b0b34cb8450affd21b7e
Tupesh <tupesghimire@gmail.com>
2024-04-04 21:06:14 +0545

    html pages update

---
+++ javascript/index.js
@@ -0,0 +1,13 @@
+function toggleForms() {
+    var toggleSwitch = document.getElementById("toggleSwitch");
+    var requestForm = document.getElementById("requestForm");
+    var donateForm = document.getElementById("donateForm");
+
+    if (toggleSwitch.checked) {
+        requestForm.style.display = "none";
+        donateForm.style.display = "block";
+    } else {
+        requestForm.style.display = "block";
+        donateForm.style.display = "none";
+    }
+}
