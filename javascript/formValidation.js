ad79bc42939d49030146d9f0cc367b845f4e5bb0
Tupesh <tupesghimire@gmail.com>
2024-04-12 17:22:57 +0545

    form validation

---
+++ javascript/formValidation.js
@@ -0,0 +1,90 @@
+// formValidation.js
+
+function validateRequestForm() {
+
+    var patientName = document.getElementById("patientName").value;
+    var hospital = document.getElementById("hospital").value;
+    var bloodGroup = document.getElementById("bloodGroup").value;
+    var bloodRh = document.getElementById("bloodRh").value;
+    var urgency = document.getElementById("urgency").value;
+
+
+    if (patientName === "") {
+        alert("Patient's name should be entered.");
+        return false;
+    }
+
+    if (hospital === "") {
+        alert("Enter hospital name.");
+        return false;
+    }
+
+    if (bloodGroup === "" || bloodRh === "") {
+        alert("Please select blood group and RH.");
+        return false;
+    }
+
+    if (urgency === "") {
+        alert("Update the urgency status.");
+        return false;
+    }
+    return true;
+
+}
+
+
+
+function validateDonorForm() {
+    var donarName = document.getElementById("donarName").value;
+    var donarBloodGroup = document.getElementById("donarBloodGroup").value;
+    var donarBloodRh = document.getElementById("donarBloodRh").value;
+    var donationDate = document.getElementById("donationDate").value;
+    var age = document.getElementById("age").value;
+    var idCard = document.getElementById("idCard").value;
+
+    if (donarName == "") {
+        alert("Donor's name should be entered.");
+        return false;
+    }
+
+    if (age == -"") {
+        alert("Enter donar's age.");
+        return false;
+    }
+    else if (age <= 18) {
+        alert("You are not eligible to donate blood");
+        return false;
+    }
+
+    if (donarBloodGroup === "" || donarBloodRh === "") {
+        alert("Please select donor's blood group and RH.");
+        return false;
+    }
+
+    if (donationDate === "") {
+        alert("Please select donation date.");
+        return false;
+    }
+
+
+
+
+    for (var i = 0; i < genderRadios.length; i++) {
+        if (genderRadios[i].checked) {
+            genderSelected = true;
+            break;
+        }
+    }
+
+    if (!genderSelected) {
+        alert("Please select donor's gender.");
+        return false;
+    }
+
+    if(idCard.isDefaultNamespace.length===0){
+        alert("Please input identity image file.");
+        return false;
+    }
+
+    return true;
+}
